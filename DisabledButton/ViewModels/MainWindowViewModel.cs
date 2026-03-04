10b9a63f2bc40558b88313f21e1913054b82aa70
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-03-04 23:09:16 +0800

    issue: add DisabledButton.

---
+++ DisabledButton/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace DisabledButton.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ DisabledButton/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DisabledButton.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
